5284f65e3388ccbd46b5c52c47cf4a0435307d40
Tanaca <jtanacar@gmail.com>
2021-07-20 12:07:40 -0300

    💄 change fonts to Montserrat

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,6 +3,6 @@ import { createGlobalStyle } from 'styled-components'
 export default createGlobalStyle`
   html{
     scroll-behavior: smooth;
-    font-family: 'Poppins', sans-serif;
+    font-family: 'Montserrat', sans-serif;
   }
 `
