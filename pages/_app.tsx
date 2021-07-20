b95cc64daf9642805320a6a90c16073d49b4d8de
Tanaca <jtanacar@gmail.com>
2021-07-20 10:07:05 -0300

    💄 ✨ add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import Global from '../src/styles/global'
 import type { AppProps } from 'next/app'
+import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
