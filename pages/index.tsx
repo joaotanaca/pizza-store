0276719342e3615ef504a6fa1796a40eebc7222d
Tanaca <jtanacar@gmail.com>
2021-07-21 14:33:07 -0300

    💄 create pill component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
+import useSWR from 'swr'
+import Pill from '../src/moleculas/Pill'
 import Hero from '../src/organismos/Hero'
 import Navbar from '../src/organismos/Navbar'
 
 export default function Home() {
+  const { data, error } = useSWR('/api/categories', { initialData: [] })
   return (
     <>
       <Navbar />
       <Hero />
+      {data?.map((category: string) => (
+        <Pill id={category.toLowerCase()} key={category} active={false}>
+          {category}
+        </Pill>
+      ))}
     </>
   )
 }
---
+++ src/moleculas/Pill/index.tsx
@@ -0,0 +1,35 @@
+import React, { HTMLAttributes } from 'react'
+import styled from 'styled-components'
+
+type TProps = HTMLAttributes<HTMLAnchorElement> & {
+  active: boolean
+  id: string
+}
+
+const PillContainer = styled.a`
+  background-color: #eceef6;
+  color: ${(props) => props.theme.text};
+  border-radius: 45px;
+  padding: 17px 40px;
+  font-weight: 600;
+  font-size: 16px;
+  &:target,
+  &.active {
+    background-color: ${(props) => props.theme.secondary};
+    color: #fff;
+  }
+`
+
+const Pill: React.FC<TProps> = ({ children, active, className, ...props }) => {
+  return (
+    <PillContainer
+      href={`#${props.id}`}
+      className={`${className}${active && 'active'}`}
+      {...props}
+    >
+      {children}
+    </PillContainer>
+  )
+}
+
+export default Pill
