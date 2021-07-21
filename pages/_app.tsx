e10af0378df640ecee4c32afd70d2c56f7c93596
Tanaca <jtanacar@gmail.com>
2021-07-21 14:33:43 -0300

    📦 add lib swr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,28 @@
+import 'tailwindcss/tailwind.css'
 import Global from '../src/styles/global'
 import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
+import { SWRConfig } from 'swr'
+import axios from 'axios'
 import { ThemeProvider } from 'styled-components'
 import { light } from '../src/styles/theme'
 
+const fetcher = async (url: string) => {
+  try {
+    const res = await axios.get(url)
+    return res.data
+  } catch (err) {
+    throw err.response.data
+  }
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SWRConfig value={{ fetcher }}>
       <ThemeProvider theme={light}>
         <Global />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </SWRConfig>
   )
 }
 export default MyApp
