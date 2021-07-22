505445dd0e1d7b08efaa36c32da1285facf208d4
Tanaca <jtanacar@gmail.com>
2021-07-22 16:03:53 -0300

    💄 replace navbar to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,13 @@ import Global from '../src/styles/global'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { light } from '../src/styles/theme'
+import Navbar from '../src/components/organismos/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider theme={light}>
+        <Navbar />
         <Global />
         <Component {...pageProps} />
       </ThemeProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Hero from '../src/components/organismos/Hero'
-import Navbar from '../src/components/organismos/Navbar'
 import SaleContent from '../src/components/organismos/SaleContent'
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <SaleContent />
     </>
