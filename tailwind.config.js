be6d83b9668d6f893361a8ce5185915bd94ef50a
Tanaca <jtanacar@gmail.com>
2021-07-22 10:31:02 -0300

    💄 add padding to container class of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      padding: { DEFAULT: '20px', sm: '40px' },
+    },
     extend: {},
   },
   variants: {
